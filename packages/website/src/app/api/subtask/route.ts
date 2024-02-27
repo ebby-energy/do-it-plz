f91ebf00957e2f45f6fae41e397ec539e22fd396
baldyeagle <austin.taylor.baldwin@gmail.com>
2024-02-26 21:36:19 -0800

    Update message

--- packages/website/src/app/api/subtask/route.ts
+++ packages/website/src/app/api/subtask/route.ts
@@ -2,6 +2,7 @@ import { withAxiom, type AxiomRequest } from "next-axiom";
 
 export const POST = withAxiom(async (req: AxiomRequest) => {
   const body = await req.json();
-  req.log.info("POSTING with data", { body });
+  const log = req.log.with({ body });
+  log.info("POSTING with data");
   return Response.json({ success: true });
 });
