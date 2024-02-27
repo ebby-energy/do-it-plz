552bb2a29c2c90fb82422b716e8d7edf8fe12f04
baldyeagle <austin.taylor.baldwin@gmail.com>
2024-02-26 21:07:58 -0800

    Use `next-axiom` for route handler

--- packages/website/src/app/api/subtask/route.ts
+++ packages/website/src/app/api/subtask/route.ts
@@ -1,5 +1,7 @@
-export async function POST(request: Request) {
-  const body = await request.json();
-  console.log("POSTING with request", { request, body });
+import { withAxiom, type AxiomRequest } from "next-axiom";
+
+export const POST = withAxiom(async (req: AxiomRequest) => {
+  const body = await req.json();
+  req.log.info("POSTING with data", { body });
   return Response.json({ success: true });
-}
+});
