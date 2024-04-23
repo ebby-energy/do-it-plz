f28e7fa56a7000d7419c33d3927a0a9742c43793
baldyeagle <austin.taylor.baldwin@gmail.com>
2024-04-22 19:42:50 -0700

    Log headers as object

--- packages/website/src/app/api/subtasks/route.ts
+++ packages/website/src/app/api/subtasks/route.ts
@@ -4,7 +4,11 @@ export const runtime = "edge";
 
 export const POST = withAxiom(async (req: AxiomRequest) => {
   const body = await req.json();
-  const headers = new Headers(req.headers);
+  const requestHeaders = new Headers(req.headers);
+  const headers = {} as Record<string, string>;
+  requestHeaders.forEach((value, key) => {
+    headers[key] = value;
+  });
   const log = req.log.with({ body, headers });
   log.info("POSTING with data");
   return Response.json({ success: true });
