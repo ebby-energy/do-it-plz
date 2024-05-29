cd0f3b96e4a6087e1ccaa10d28c37afcffd4a14b
baldyeagle <austin.taylor.baldwin@gmail.com>
2024-05-28 23:04:07 -0700

    Show organization information in new settings page

--- packages/website/src/app/dashboard/page.tsx
+++ packages/website/src/app/dashboard/page.tsx
@@ -1,19 +1,71 @@
-import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
+import { db } from "@/db/parent";
 import { auth } from "@clerk/nextjs/server";
+import { sql } from "drizzle-orm";
+import Link from "next/link";
+import { notFound } from "next/navigation";
 
-export default function Page() {
-  const { orgId, userId } = auth();
+const getOrganization = db.query.organizations
+  .findMany({
+    columns: {
+      id: true,
+      name: true,
+      publicId: true,
+      createdAt: true,
+    },
+    where: (org, { eq }) => eq(org.id, sql.placeholder("id")),
+  })
+  .prepare();
+
+type ComponentProps = {
+  publicId: string;
+  name: string;
+};
+const Component = ({ publicId, name }: ComponentProps) => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-start p-24 pt-0">
-      <div className="flex w-screen flex-row items-center justify-between px-4 pb-24">
-        do-it-plz
-        <div className="flex h-full flex-row items-center justify-center gap-x-8">
-          <OrganizationSwitcher />
-          <UserButton />
-        </div>
-      </div>
-      <p>User Id: {userId}</p>
-      <p>Org ID: {orgId}</p>
-    </main>
+    <>
+      <p>Project: {name}</p>
+      <Link
+        className="text-foreground font-medium hover:underline"
+        href={`/dashboard/${publicId}/settings`}
+      >
+        Go to Settings
+      </Link>
+    </>
   );
+};
+
+export default async function Page() {
+  const { orgId, userId } = auth();
+
+  if (!orgId && !userId) {
+    notFound();
+  }
+
+  if (orgId) {
+    const results = await getOrganization.all({ id: orgId });
+    if (results.length === 0) {
+      return <p>No projects found for organization</p>;
+    }
+    return results.map((result) => (
+      <Component
+        key={result.publicId}
+        name={result.name}
+        publicId={result.publicId}
+      />
+    ));
+  }
+
+  if (userId) {
+    const results = await getOrganization.all({ id: userId });
+    if (results.length === 0) {
+      return <p>No projects found for user</p>;
+    }
+    return results.map((result) => (
+      <Component
+        key={result.publicId}
+        name={result.name}
+        publicId={result.publicId}
+      />
+    ));
+  }
 }
---
+++ packages/website/src/components/design/container.tsx
@@ -0,0 +1,7 @@
+export const Container = ({ children }: { children: React.ReactNode }) => (
+  <div className="mx-auto flex w-screen flex-1 flex-col items-center justify-between">
+    <div className="mx-auto flex h-full w-full max-w-3xl flex-initial flex-col items-center justify-between px-4 sm:px-16 lg:px-24">
+      {children}
+    </div>
+  </div>
+);
