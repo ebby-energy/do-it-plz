ae72c319902e104d198025f8dafa30fb8aa37d70
baldyeagle <austin.taylor.baldwin@gmail.com>
2024-06-04 20:39:32 -0700

    Change container to better support long text strings

--- packages/website/src/components/design/container.tsx
+++ packages/website/src/components/design/container.tsx
@@ -1,6 +1,6 @@
 export const Container = ({ children }: { children: React.ReactNode }) => (
   <div className="mx-auto flex w-screen flex-1 flex-col items-center justify-between">
-    <div className="mx-auto flex h-full w-full max-w-3xl flex-initial flex-col items-center justify-between px-4 py-6 sm:px-16 lg:px-24">
+    <div className="mx-auto flex h-full w-full max-w-3xl flex-initial flex-col items-center justify-between px-4 py-6 sm:px-12 lg:px-16">
       {children}
     </div>
   </div>
